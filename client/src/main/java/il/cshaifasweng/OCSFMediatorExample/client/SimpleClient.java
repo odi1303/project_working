18a377ef26170e384a323db05be032eefd149a4e
odi1303 <ofinke04@campus.haifa.ac.il>
2024-12-18 21:57:22 +0200

    the windows open and only needs to work as said

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SimpleClient.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SimpleClient.java
@@ -8,21 +8,38 @@ import il.cshaifasweng.OCSFMediatorExample.entities.Warning;
 public class SimpleClient extends AbstractClient {
 	
 	private static SimpleClient client = null;
-
+	public static String sign;
+	private static PrimaryController primaryController=new PrimaryController();
 	private SimpleClient(String host, int port) {
 		super(host, port);
 	}
 
 	@Override
 	protected void handleMessageFromServer(Object msg) {
+		String message = msg.toString();
+		System.out.println(message);
 		if (msg.getClass().equals(Warning.class)) {
 			EventBus.getDefault().post(new WarningEvent((Warning) msg));
 		}
-		else{
-			String message = msg.toString();
-			System.out.println(message);
+		else if (message.contains("client added successfully with sign ")){
+			if(message.contains("X"))
+				sign="X";
+			else if(message.contains("O"))
+				sign="O";
 		}
+		else{
+			String[][] board=(String[][]) msg;
+			primaryController.myTurn(board);
+		}
+
 	}
+	public static String[][] myTurn(String[][] givenboard){
+
+		primaryController.myTurn(givenboard);
+		givenboard=primaryController.logicBoard;
+		return givenboard;
+	}
+
 	
 	public static SimpleClient getClient() {
 		if (client == null) {
