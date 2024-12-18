f0e851354bd70483e5a6482120a34e1a22b03826
odi1303 <ofinke04@campus.haifa.ac.il>
2024-12-18 21:57:22 +0200

    the windows open and only needs to work as said

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
@@ -4,18 +4,128 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+/**
+ * Sample Skeleton for 'primary.fxml' Controller Class
+ */
+/**
+ * Sample Skeleton for 'primary.fxml' Controller Class
+ */
+
+import javafx.scene.control.Button;
+
 
 public class PrimaryController {
 
-    @FXML
-    void sendWarning(ActionEvent event) {
-    	try {
+	@FXML // fx:id="Button00"
+	private Button Button00; // Value injected by FXMLLoader
+
+	@FXML // fx:id="Button01"
+	private Button Button01; // Value injected by FXMLLoader
+
+	@FXML // fx:id="Button02"
+	private Button Button02; // Value injected by FXMLLoader
+
+	@FXML // fx:id="Button10"
+	private Button Button10; // Value injected by FXMLLoader
+
+	@FXML // fx:id="Button11"
+	private Button Button11; // Value injected by FXMLLoader
+
+	@FXML // fx:id="Button12"
+	private Button Button12; // Value injected by FXMLLoader
+
+	@FXML // fx:id="Button20"
+	private Button Button20; // Value injected by FXMLLoader
+
+	@FXML // fx:id="Button21"
+	private Button Button21; // Value injected by FXMLLoader
+
+	@FXML // fx:id="Button22"
+	private Button Button22; // Value injected by FXMLLoader
+	String [][] logicBoard =new String[3][3];
+
+	@FXML
+	void write00(ActionEvent event) throws IOException {
+		Button00.setText(SimpleClient.sign);
+		logicBoard[0][0]=Button00.getText();
+		disableBoard();
+		SimpleClient.getClient().sendToServer("0,0");
+	}
+
+	@FXML
+	void write01(ActionEvent event) throws IOException {
+		Button01.setText(SimpleClient.sign);
+		logicBoard[0][1]=Button01.getText();
+		disableBoard();
+		SimpleClient.getClient().sendToServer("0,1");
+	}
+
+	@FXML
+	void write02(ActionEvent event) throws IOException {
+		Button02.setText(SimpleClient.sign);
+		logicBoard[0][2]=Button02.getText();
+		disableBoard();
+		SimpleClient.getClient().sendToServer("0,2");
+	}
+
+	@FXML
+	void write10(ActionEvent event) throws IOException {
+		Button10.setText(SimpleClient.sign);
+		logicBoard[1][0]=Button10.getText();
+		disableBoard();
+		SimpleClient.getClient().sendToServer("1,0");
+	}
+
+	@FXML
+	void write11(ActionEvent event) throws IOException {
+		Button11.setText(SimpleClient.sign);
+		logicBoard[1][1]=Button11.getText();
+		disableBoard();
+		SimpleClient.getClient().sendToServer("1,1");
+	}
+
+	@FXML
+	void write12(ActionEvent event) throws IOException {
+		Button12.setText(SimpleClient.sign);
+		logicBoard[1][2]=Button12.getText();
+		disableBoard();
+		SimpleClient.getClient().sendToServer("1,2");
+	}
+
+	@FXML
+	void write20(ActionEvent event) throws IOException {
+		Button20.setText(SimpleClient.sign);
+		logicBoard[2][0]=Button20.getText();
+		disableBoard();
+		SimpleClient.getClient().sendToServer("2,0");
+	}
+
+	@FXML
+	void write21(ActionEvent event) throws IOException {
+		Button21.setText(SimpleClient.sign);
+		logicBoard[2][1]=Button21.getText();
+		disableBoard();
+		SimpleClient.getClient().sendToServer("2,1");
+	}
+
+	@FXML
+	void write22(ActionEvent event) throws IOException {
+		Button22.setText(SimpleClient.sign);
+		logicBoard[2][2]=Button22.getText();
+		disableBoard();
+		SimpleClient.getClient().sendToServer("2,2");
+
+	}
+
+	@FXML
+	void sendWarning(ActionEvent event) {
+		try {
 			SimpleClient.getClient().sendToServer("#warning");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
 	@FXML
 	void initialize(){
@@ -25,5 +135,55 @@ public class PrimaryController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		disableBoard();
+
+	}
+	@FXML
+	void myTurn(String[][]board){
+		updateButtons();
+		logicBoard=board;
+		disableBoard();//the turn is over
+
+	}
+	@FXML
+	void updateButtons(){
+		Button00.setText(logicBoard[0][0]);
+		if (Button00.getText()==" ")
+			Button00.setDisable(false);
+		Button01.setText(logicBoard[0][1]);
+		if (Button01.getText()==" ")
+			Button01.setDisable(false);
+		Button02.setText(logicBoard[0][2]);
+		if (Button02.getText()==" ")
+			Button02.setDisable(false);
+		Button10.setText(logicBoard[1][0]);
+		if (Button10.getText()==" ")
+			Button10.setDisable(false);
+		Button11.setText(logicBoard[1][1]);
+		if (Button11.getText()==" ")
+			Button11.setDisable(false);
+		Button12.setText(logicBoard[1][2]);
+		if (Button12.getText()==" ")
+			Button12.setDisable(false);
+		Button20.setText(logicBoard[2][0]);
+		if (Button20.getText()==" ")
+			Button20.setDisable(false);
+		Button21.setText(logicBoard[2][1]);
+		if (Button21.getText()==" ")
+			Button21.setDisable(false);
+		Button22.setText(logicBoard[2][2]);
+		if (Button22.getText()==" ")
+			Button22.setDisable(false);
+	}
+	void disableBoard(){
+		Button00.setDisable(true);
+		Button01.setDisable(true);
+		Button02.setDisable(true);
+		Button10.setDisable(true);
+		Button11.setDisable(true);
+		Button12.setDisable(true);
+		Button20.setDisable(true);
+		Button21.setDisable(true);
+		Button22.setDisable(true);
 	}
 }
--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
@@ -11,11 +11,24 @@ import il.cshaifasweng.OCSFMediatorExample.server.ocsf.SubscribedClient;
 
 public class SimpleServer extends AbstractServer {
 	private static ArrayList<SubscribedClient> SubscribersList = new ArrayList<>();
+	private static SubscribedClient[] subscribers = new SubscribedClient[SubscribersList.size()];
+	private int numberOfSubscribers = 0;
+	private String []signs={"X","O"};
+	public String[][] board = new String[3][3];
+	public String sign;
 
 	public SimpleServer(int port) {
 		super(port);
-		
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				board[i][j] = " ";
+			}
+		}
 	}
+	public String[][] getBoard() {
+		return board;
+	}
+
 
 	@Override
 	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
@@ -33,10 +46,11 @@ public class SimpleServer extends AbstractServer {
 			SubscribedClient connection = new SubscribedClient(client);
 			SubscribersList.add(connection);
 			try {
-				client.sendToClient("client added successfully");
+				client.sendToClient("client added successfully with sign "+signs[numberOfSubscribers]);
 			} catch (IOException e) {
 				throw new RuntimeException(e);
 			}
+			numberOfSubscribers++;
 		}
 		else if(msgString.startsWith("remove client")){
 			if(!SubscribersList.isEmpty()){
@@ -47,6 +61,8 @@ public class SimpleServer extends AbstractServer {
 					}
 				}
 			}
+		} else if (msgString.length()==3) {
+			board[msgString.charAt(0)][msgString.charAt(2)] = sign;
 		}
 	}
 	public void sendToAllClients(String message) {
@@ -58,5 +74,50 @@ public class SimpleServer extends AbstractServer {
 			e1.printStackTrace();
 		}
 	}
+	public void game_manegment() throws IOException {
+		ConnectionToClient tempClient;
+		for(int i=0; i<9; i++){
+			sign=signs[i%2];
+			sendToAllClients(board);
+			subscribers[i%2].getClient().sendToClient("your turn");
+
+		}
+	}
+	//checking if there's a win in the current board
+	public boolean checkForWin(){
+		if (checkHorizontal()||checkVertical()||checkDiagonal())
+			return true;
+		return false;
+	}
+	//checking for win in horizontal way
+	public boolean checkHorizontal(){
+		for (int i = 0; i < 3; i++) {
+			if (board[i][0]==board[i][1] && board[i][1]==board[i][2] && board[i][0]!=" ") {
+				return true;
+			}
+		}
+		return false;
+	}
+	//checking for win in vertical way
+	public boolean checkVertical(){
+		for (int i = 0; i < 3; i++) {
+			if (board[0][i]==board[1][i] && board[1][i]==board[2][i] && board[0][i]!=" ") {
+				return true;
+			}
+		}
+		return false;
+	}
+	//checking for win in diagonal way
+	public boolean checkDiagonal(){
+		if (board[0][0]==board[1][1] && board[1][1]==board[2][2] && board[2][2]!=" ") {
+			return true;
+		}
+		else if (board[0][2]==board[1][1] && board[1][1]==board[2][0] && board[0][2]!=" ") {
+			return true;
+		}
+		else
+			return false;
+	}
+
 
 }
