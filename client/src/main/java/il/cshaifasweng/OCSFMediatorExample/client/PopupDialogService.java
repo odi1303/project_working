5bb7499e56587cac68270c7be73c4c150441f7b9
Kiran Kabaso <kirankabaso@gmail.com>
2025-03-23 15:59:24 +0200

    added PopupController and PopupDialogService files, used when creating any popup scene

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PopupDialogService.java
@@ -0,0 +1,30 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PopupDialogService {
+
+    public <T extends PopupController> String openPopup(String fxml, String input, Stage ownerStage) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+        Parent root = loader.load(); //load fxml
+        T controller = loader.getController();  //get fxml controller
+
+        Stage popupStage = new Stage();
+        popupStage.initModality(Modality.WINDOW_MODAL);
+        popupStage.initOwner(ownerStage); //set it so the pop-up window blocks actions in the calling window
+
+        controller.reInitialize(input); //use reInitialize to pass the input to the scene controller and reinitialize the scene with the input.
+        popupStage.setScene(new Scene(root)); //show the fxml in the popup window, not visible until now.
+        //popupStage.setResizable(false); //blocks changing the pop-up window location, shape and size.
+
+        popupStage.showAndWait();
+
+        return controller.getOutput();
+    }
+}
