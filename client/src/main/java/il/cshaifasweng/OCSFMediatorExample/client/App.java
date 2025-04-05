8656329c18df784c3ee672b48948d2c5484ac7c8
Ronnie Rasamat <delta7.athersprite@gmail.com>
2025-04-06 00:44:23 +0300

    server progress 8

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/App.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/App.java
@@ -64,6 +64,14 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        EventBus.getDefault().register(this);
+        client = SimpleClient.getClient();
+        client.openConnection();
+        client.sendToServer("add client"); // לא באמת יודע איפה לשים את השורה הזו, באב טיפוס שמנו אותה בINIT של הCONTROLLER הראשון
+
+        scene = new Scene(loadFXML("home-page"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
