6689a51805502d2812c06c6545f23a712676b544
Kiran Kabaso <kirankabaso@gmail.com>
2025-04-05 18:26:22 +0300

    added harcoded method in client to show reqired fee to pay on delivery calcelation

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/CancelDeliveryScreenController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/CancelDeliveryScreenController.java
@@ -40,7 +40,7 @@ public class CancelDeliveryScreenController {
 
         //add cancel button
         Button cancelButton = new Button("Cancel order");
-        cancelButton.setOnAction(event -> removeOrder(scrollPane));
+        cancelButton.setOnAction(event -> removeOrder(order, scrollPane));
         hBox.getChildren().add(cancelButton);
 
         for (Pair<DishClient, Integer> pair : order.getOrderDishesList()){
@@ -86,12 +86,12 @@ public class CancelDeliveryScreenController {
         return hBox;
     }
 
-    private void removeOrder(ScrollPane scrollPane) {
+    private void removeOrder(OrderClient order, ScrollPane scrollPane) {
         PopupDialogService popupDialogService = new PopupDialogService();
         try {
             boolean isConfirmed = popupDialogService.openPopup("ConfirmationWindow.fxml", "are you sure you want to delete the order?", (Stage) orderTable.getScene().getWindow());
             if (isConfirmed) {
-                boolean confirmed = popupDialogService.openPopup("ConfirmationWindow.fxml", "you will be required to pay ___.", (Stage) orderTable.getScene().getWindow());
+                boolean confirmed = popupDialogService.openPopup("ConfirmationWindow.fxml", "you will be required to pay: " + String.valueOf(getRequiredCancelationFee(order)), (Stage) orderTable.getScene().getWindow());
                 if (confirmed) {
                     orderTable.getChildren().remove(scrollPane);
                 }
@@ -99,5 +99,9 @@ public class CancelDeliveryScreenController {
         }catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private double getRequiredCancelationFee(OrderClient order) {
+        return 5.0;
     }
 }
