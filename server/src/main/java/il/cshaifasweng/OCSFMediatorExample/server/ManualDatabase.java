1651441812152ba68d2d621fb2ff77bb518f7955
Coder-Artist <52375055+LeafGreenRemake@users.noreply.github.com>
2025-04-27 19:45:55 +0300

    Update ManualDatabase.java

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/ManualDatabase.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/ManualDatabase.java
@@ -47,16 +47,16 @@ public class ManualDatabase {
     }
 
     public ManualDatabase() {
-//        try {
-//            SessionFactory sessionFactory = getSessionFactory();
-//            session = sessionFactory.openSession();
-//        } catch (Exception exception) {
-//            if (session != null) {
-//                session.getTransaction().rollback();
-//            }
-//            System.err.println("An error occurred, changes have been rolled back.");
-//            exception.printStackTrace();
-//        }
+        try {
+            SessionFactory sessionFactory = getSessionFactory();
+            session = sessionFactory.openSession();
+        } catch (Exception exception) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            System.err.println("An error occurred, changes have been rolled back.");
+            exception.printStackTrace();
+        }
     }
 
     public void saveOrUpdate(Object o) {
