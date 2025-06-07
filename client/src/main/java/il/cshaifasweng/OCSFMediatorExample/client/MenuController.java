779b07a3847ad53f23dd743c08158b676c575057
Kiran Kabaso <kirankabaso@gmail.com>
2025-06-07 18:26:06 +0300

    small merge fixes

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/MenuController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/MenuController.java
@@ -199,7 +199,7 @@ public class MenuController {
         }else{
             currentMenu = null;
         }
-    //});
+        //});
     }
 
     //good
@@ -370,7 +370,7 @@ public class MenuController {
     @FXML
     private void finishOrder() throws IOException {
         PopupDialogService popupDialogService = new PopupDialogService();
-        if (getDishesCountPair().isEmpty()){
+        if (IsOrderEmpty()){
             System.out.println("Your cart is empty!");
             popupDialogService.openPopup("InformationWindow.fxml", "Your cart is empty!", (Stage) orderSection.getScene().getWindow());
             return;
@@ -380,8 +380,11 @@ public class MenuController {
             if(canTheOrderBeMadeFromASingleBranch(dishesInOrder)){
                 Boolean isConfirmed = popupDialogService.openPopup("ConfirmationWindow.fxml", "Finish Order?", (Stage) orderSection.getScene().getWindow());
                 if (isConfirmed != null && isConfirmed) {
-                    LocationInformation locationInfo = getLocationInformation();
                     boolean isDelivery = getIsDelivery();
+                    LocationInformation locationInfo = null;
+                    if (isDelivery) {
+                        locationInfo = getLocationInformation();
+                    }
                     personalInformation = popupDialogService.openPopup("PersonalInformationPopupWindow.fxml", null, (Stage) orderSection.getScene().getWindow());
                     if (personalInformation != null) {
                         CreditInformation creditInformation = popupDialogService.openPopup("CreditInformationPopupWindow.fxml", null, (Stage) orderSection.getScene().getWindow());
@@ -389,7 +392,7 @@ public class MenuController {
                         if (creditInformation != null) {
                             Boolean Confirmed = popupDialogService.openPopup("ConfirmationWindow.fxml", "Total price is:" + String.valueOf(getTotalPrice()) + ". Confirm Order?", (Stage) orderSection.getScene().getWindow());
                             if (Confirmed != null && Confirmed) {
-                                OrderClient order = new OrderClient(getDishesCountPair(), isDelivery, locationInfo, personalInformation, creditInformation);
+                                OrderClient order = createOrderClient(isDelivery, locationInfo, personalInformation, creditInformation);
                                 sendOrder(order);
                             }
                         }
@@ -470,7 +473,7 @@ public class MenuController {
         return totalPrice;
     }
 
-//    //probebly good
+    //    //probebly good
 //    private ArrayList<OrderItem> getDishesCountPair(){
 //        ArrayList<OrderItem> pairList = new ArrayList<>();
 //        for (OrderItem pair : orderDishNodeControllerCountLabelPair){
